213d0274c0c58fc5cdf81b97923fe529cdf12600
poppyz <xinru1995@outlook.com>
2019-12-27 14:12:39 +0800

    add Sort Code

---
+++ Algorithm/Code/Sort.java
@@ -0,0 +1,109 @@
+import java.util.*;
+
+public class Main {
+    public static <T extends Comparable> T[] bubbleSort(T[] array)
+    {
+        T [] result = array.clone();
+        int count = result.length;
+        for(int i = 0 ;i<count;i++)
+        {
+            T temp;
+            for(int j = 0 ;j<result.length - 1 - i;j++)
+            {
+                if (result[j].compareTo(result[j+1])>0)
+                {
+                    temp = result[j];
+                    result[j] = result[j+1];
+                    result[j+1] = temp;
+                }
+            }
+        }
+        return result;
+    }
+    public static <T extends Comparable> T[] insertionSort(T[] array)
+    {
+        T [] result = array.clone();
+        for (int i = 1; i < result.length; i++)
+        {
+            T temp;
+            for (int j = 0; j < i; j++)
+            {
+                if (result[i].compareTo(result[j]) >= 0)
+                {
+
+                }
+                else
+                {
+                    for (int t = i; t> j; t--)
+                    {
+                        temp = result[t];
+                        result[t] = result[t-1];
+                        result[t-1] = temp;
+                    }
+                }
+            }
+        }
+        return  result;
+    }
+    public static <T extends Comparable> T[] selectionSort(T[] array)
+    {
+        T [] result = array.clone();
+        T max;
+        for (int i = 0; i < result.length; i++)
+        {
+            max = result[0];
+            int no = 0;
+            for(int j = 0; j < result.length - i; j++)
+            {
+                if (max.compareTo(result[j]) < 0)
+                {
+                    max = result[j];
+                    no = j;
+                }
+            }
+            result[no] = result[result.length-i-1];
+            result[result.length-i-1] = max;
+        }
+        return result;
+    }
+
+    public static <T extends Comparable> boolean check(T[] array)
+    {
+        for(int i = 0; i < array.length -1; i++)
+        {
+            if (array[i].compareTo(array[i+1]) > 0 )
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Random r1 = new Random();
+        Integer [] array = new Integer[10000];
+        for (int i = 0; i< array.length; i++)
+        {
+            array[i] = r1.nextInt(10000000);
+        }
+        System.out.println("Raw   "+Arrays.toString(array));
+
+
+        long startTime=System.currentTimeMillis();
+        Integer [] bubbleResult = bubbleSort(array);
+        long bubbleTime = System.currentTimeMillis() - startTime;
+        startTime=System.currentTimeMillis();
+        Integer [] insertionResult = insertionSort(array);
+        long insertionTime = System.currentTimeMillis() - startTime;
+        startTime=System.currentTimeMillis();
+        Integer [] selectionResult = selectionSort(array);
+        long selectionTime = System.currentTimeMillis() - startTime;
+
+
+
+        System.out.println("bubble:     "+check(bubbleResult)+"  time "+bubbleTime+" ms");
+        System.out.println("insertion:  "+check(insertionResult)+"  time "+insertionTime+" ms");
+        System.out.println("selection:  "+check(selectionResult)+"  time "+selectionTime+" ms");
+    }
+}
